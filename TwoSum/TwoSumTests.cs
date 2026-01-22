2d489cee370ddfef88605e5decd9ce4b6212290a
Nguyễn Trung Sơn <126353303+SadNguyen2107@users.noreply.github.com>
2026-01-23 01:04:43 +0700

    Create Unit Test for TwoSum Problem

---
+++ TwoSum/TwoSumTests.cs
@@ -0,0 +1,49 @@
+using LeetCode;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public sealed class TwoSumTests
+    {
+        private TwoSumSolution twoSum;
+
+        [TestInitialize]
+        public void Init()
+        {
+            twoSum = new TwoSumSolution();
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] expectedResult = { 0, 1 };
+            int[] result = twoSum.TwoSum(nums, target);
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            int[] nums = { 3, 2, 4 };
+            int target = 6;
+
+            int[] expectedResult = { 1, 2 };
+            int[] result = twoSum.TwoSum(nums, target);
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            int[] nums = { 3, 3 };
+            int target = 6;
+
+            int[] expectedResult = { 0, 1 };
+            int[] result = twoSum.TwoSum(nums, target);
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+    }
+}
