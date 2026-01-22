da95db5427d9e930d35fd548382d53513b70bab8
Nguyễn Trung Sơn <126353303+SadNguyen2107@users.noreply.github.com>
2026-01-23 01:01:51 +0700

    Create TwoSum Solution

---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            // If the dictionary contain complement
+            // Return that index + map[complement]
+            if (map.ContainsKey(complement))
+            {
+                return new int[2] { map[complement], i };
+            }
+
+            // If that dictionary does not contains that number
+            // Add to the dictionary
+            map[nums[i]] = i;
+        }
+
+        return new int[0];
+    }
+}
