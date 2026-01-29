3cdbef24766a9de1b2320441efc024f77310cc32
Nguyễn Trung Sơn <126353303+SadNguyen2107@users.noreply.github.com>
2026-01-29 20:39:34 +0700

    Add unit tests for Median of Two Sorted Arrays

---
+++ 4. Median of Two Sorted Arrays/MedianOfTwoSortedArraysTests.cs
@@ -0,0 +1,60 @@
+using LeetCode;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class MedianOfTwoSortedArraysTests
+    {
+        private MedianOfTwoSortedArraysSolution _solution;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _solution = new MedianOfTwoSortedArraysSolution();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] nums1 = { 1, 3 };
+            int[] nums2 = { 2 };
+
+            const double expected = 2;
+            double actual = _solution.FindMedianSortedArrays(nums1, nums2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] nums1 = { 1, 2 };
+            int[] nums2 = { 3, 4 };
+
+            const double expected = 2.5;
+            double actual = _solution.FindMedianSortedArrays(nums1, nums2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] nums1 = { 1, 2, 3, 4, 5 };
+            int[] nums2 = { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+
+            const double expected = 9.00000;
+            double actual = _solution.FindMedianSortedArrays(nums1, nums2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] nums1 = { -10, -9, -8 };
+            int[] nums2 = { 1, 2 };
+
+            const double expected = -8;
+            double actual = _solution.FindMedianSortedArrays(nums1, nums2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
